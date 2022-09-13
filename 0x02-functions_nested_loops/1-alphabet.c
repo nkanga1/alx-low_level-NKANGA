fb94ddd91890f07bfd498a64f960a2f985966ba6
nkanga1 <edunkanga@gmail.com>
2022-09-13 16:30:42 -0700

    alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,18 +2,21 @@
 
 /**
  * print_alphabet - Entry point
- * Description: a function that prints the alphabet,
- * in lowecase followed by a new line
- * Return: void
+ * Return:0
  */
+
 void print_alphabet(void)
 {
-	char letter = 'a';
+	char c = 'a';
 
-	while (letter <= 'z')
+	while (c <= 'z')
 	{
-		_putchar(letter);
-		letter++;
+		_putchar(c);
+		c++;
+
 	}
 	_putchar('\n');
+
 }
+Footer
+© 2022 GitHub, Inc.

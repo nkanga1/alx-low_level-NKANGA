3113d645fa425722f8105d97425f2acfc41c5df7
nkanga1 <edunkanga@gmail.com>
2022-10-06 17:51:07 -0700

    2-callo.c

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,11 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
-
-int _putchar(char c);
-void *malloc_checked(unsigned int b);
+int _putchar(char ch);
+void *malloc_checked(unsigned int b); 
 char *string_nconcat(char *s1, char *s2, unsigned int n);
-void *_calloc(unsigned int nmedb, unsigned int size);
-int *array_range(int min, int max);
+void *_calloc(unsigned int nmemb, unsigned int size);
+void *_calloc(unsigned int nmemb, unsigned int size);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+
 
 #endif
